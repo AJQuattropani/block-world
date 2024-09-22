152cb5131028099e104661af3e62d461f61f9077
Aurora <ajenningsquattrop99@unm.edu>
2024-09-22 04:29:07 -0600

    Ensured the header file remained unchanged.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include "glad/glad.h"
 #include <GLFW/glfw3.h>
 
 int main()
@@ -12,6 +13,15 @@ int main()
 
 	window = glfwCreateWindow(640, 480, "BlockWorld", NULL, NULL);
 	glfwMakeContextCurrent(window);
+	
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Couldn't load GLAD." << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glClearColor(0.54f, 0.3f, 0.7f, 1.0f);
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
